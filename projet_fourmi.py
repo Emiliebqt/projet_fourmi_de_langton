32f7f9fbe2d0c5faaa2e5c2300d5cc515186777e
Emilie BOQUET <emilie.boquet@ens.uvsq.fr>
2022-05-09 22:34:45 +0200

    projet

--- projet_fourmi.py
+++ projet_fourmi.py
@@ -6,6 +6,11 @@ import tkinter as tk
 def demarer():
     pass
 
+def next():
+    pass
+
+def pause():
+    pass
 
 #programme principal 
 
@@ -14,13 +19,18 @@ root.title("Fourmi de Langton")
 #creation du canvas 
 canvas=tk.Canvas(root, width=700, height=300, bg="white")
 
-bouton_demarer=tk.Button(root, text="demarer")
+bouton_play=tk.Button(root, text="play", command=demarer)
+bouton_next=tk.Button(root, text="next", command=next)
+bouton_pause=tk.Button(root, text="pause", command=pause)
+
 
 #definition des widgets 
 
 canvas.grid(row=1, rowspam=10, column=1, columnspam=10)
 
-bouton_demarer.grid(row=1, column=10)
+bouton_play.grid(row=1, column=10)
+bouton_next.grid(row=1, column=10)
+bouton_pause.grid(row=1, column=10)
 
  
 
