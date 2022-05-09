3c1668dbc274bfa0fa61f009ec3ebf42e691bee5
Emilie BOQUET <emilie.boquet@ens.uvsq.fr>
2022-05-09 23:44:09 +0200

    projet

--- projet_fourmi.py
+++ projet_fourmi.py
@@ -12,6 +12,21 @@ def next():
 def pause():
     pass
 
+def invetape():
+    global play, compteur, labelCompteur, varCheckDraw
+    compteur-=1#on incremente le compteur
+    labelCompteur.config(text="Compteur : "+str(compteur))#on affiche le nouveau texte du compteur
+    tilt.interactWithCell()#la fourmi interagie pour changer de direction
+    if(varCheckDraw.get()==1):#si on doit dessiner 
+        drawCell(tilt.pos[0], tilt.pos[1], canvas)#on dessine la case ou se trouvait la fourmi pour effacer le triangle
+        tilt.invwalk()#on fait avancer la fourmi
+        tilt.draw(canvas)#on dessine sa nouvelle position
+    else:
+        tilt.invwalk()#sinon on ne dessine pas on fait juste avancer la fourmi
+    
+    fen.update_idletasks()#utilisé pour mettre à jour les evenements de la fenetre
+    fen.update()
+
 #programme principal 
 
 root=tk.Tk()
