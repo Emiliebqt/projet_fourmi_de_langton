e21c3992ac5bcb4b76f6d75766c9fd44d09d3057
Emilie BOQUET <emilie.boquet@ens.uvsq.fr>
2022-05-09 18:00:11 +0200

    projet

---
+++ projet_fourmi.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+#constantes
+
+#fonctions 
+def demarer():
+    pass
+
+
+#programme principal 
+
+root=tk.Tk()
+root.title("Fourmi de Langton")
+#creation du canvas 
+canvas=canvas(root, width=700, height=300, bg="white")
+
+bouton_demarer=tk.Button(root, text="demarer")
+
+#definition des widgets 
+
+canvas.grid(row=1, rowspam=10, column=1, columnspam=10)
+
+bouton_demarer.grid(row=1, column=10)
+
+
+
+
